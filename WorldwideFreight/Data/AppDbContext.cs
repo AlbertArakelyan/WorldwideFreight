1c2b293a38fb74ba1c0af3768115e25bf1a2dea8
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-02-02 21:53:40 +0400

    created user model and users table

--- WorldwideFreight/Data/AppDbContext.cs
+++ WorldwideFreight/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WorldwideFreight.Models;
 
 namespace WorldwideFreight.Data
 {
@@ -6,6 +7,42 @@ namespace WorldwideFreight.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        DbSet<User> Users { get; set; }
+
+        public override int SaveChanges()
+        {
+            ApplyTimestapms();
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            ApplyTimestapms();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void ApplyTimestapms()
+        {
+            var now = DateTime.UtcNow;
+
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = now;
+                    entry.Entity.UpdatedAt = now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    // Prevent CreatedAt from being modified
+                    entry.Property(x => x.CreatedAt).IsModified = false;
+
+                    entry.Entity.UpdatedAt = now;
+                }
+            }
         }
     }
 }
