9307d294db84354cace9c995b29f65bb87177438
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-02-07 14:49:19 +0400

    created GetCarriers API in CarrierController

--- WorldwideFreight/Controllers/CarrierController.cs
+++ WorldwideFreight/Controllers/CarrierController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WorldwideFreight.Data;
 using WorldwideFreight.Dtos.ApiDtos;
 using WorldwideFreight.Dtos.Carrier;
+using WorldwideFreight.Dtos.CommodityDtos;
 using WorldwideFreight.Models;
 
 namespace WorldwideFreight.Controllers;
@@ -67,7 +69,54 @@ public class CarrierController : BaseController
         {
             Console.WriteLine(ex);
             _logger.LogError(ex, "Error creating carrier.");
-            return StatusCode(500, "An error occurred while creating the carrier.");
+            return StatusCode(500, Ok(new ApiResponseDto<List<Carrier>>
+            {
+                Success = true,
+                Message = "Carriers retrieved successfully.",
+            }));
+        }
+    }
+
+    [HttpGet]
+    [Authorize()]
+    public async Task<IActionResult> GetCarriers()
+    {
+        try
+        {
+            var carriers = await _dbContext.Carriers
+                .Include(c => c.Commodity)
+                .Select(c => new GetCarriersResponseDto
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    LogoUrl = c.LogoUrl,
+                    Commodity = c.Commodity == null ? null : new CreateCommodityResponse
+                    {
+                        Id = c.Commodity.Id,
+                        Name = c.Commodity.Name,
+                        Code = c.Commodity.Code
+                    },
+                    CreatedAt = c.CreatedAt,
+                    UpdatedAt = c.UpdatedAt
+                })
+                .ToListAsync();
+            
+            return Ok(new ApiResponseDto<List<GetCarriersResponseDto>>
+            {
+                Success = true,
+                Message = "Carriers retrieved successfully.",
+                Data = carriers
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            _logger.LogError(ex, "Error retrieving carriers.");
+            return StatusCode(500, new ApiResponseDto<object>
+            {
+                Success = false,
+                Message = "An error occurred while processing your request."
+            });
         }
     }
 }
---
+++ WorldwideFreight/Dtos/Carrier/GetCarriersResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using WorldwideFreight.Dtos.CommodityDtos;
+
+namespace WorldwideFreight.Dtos.Carrier;
+
+public class GetCarriersResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LogoUrl { get; set; }
+    public CreateCommodityResponse Commodity { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; } 
+}
